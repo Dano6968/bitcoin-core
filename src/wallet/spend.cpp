8108996390631d2070615719d2b46bf5350b5115
furszy <matiasfurszyfer@protonmail.com>
2022-04-19 15:13:33 -0300

    wallet: 'AttemptSelection -> GroupOutputs', use already cached ancestors and descendants count.
    As the wallet can fallback to try different UTXO selection behaviors, it was ending up locking the mempool mutex two times per available UTXO for each 'AttemptSelection/GroupOutputs' call.

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -338,8 +338,11 @@ std::vector<OutputGroup> GroupOutputs(const CWallet& wallet, const std::vector<C
             // Skip outputs we cannot spend
             if (!output.spendable) continue;
 
-            size_t ancestors, descendants;
-            wallet.chain().getTransactionAncestry(output.outpoint.hash, ancestors, descendants);
+            size_t ancestors{0}, descendants{0};
+            if (output.m_mempool_info) {
+                ancestors = output.m_mempool_info->ancestors_count;
+                descendants = output.m_mempool_info->descendants_count;
+            }
 
             // Make an OutputGroup containing just this output
             OutputGroup group{coin_sel_params};
@@ -363,8 +366,11 @@ std::vector<OutputGroup> GroupOutputs(const CWallet& wallet, const std::vector<C
         // Skip outputs we cannot spend
         if (!output.spendable) continue;
 
-        size_t ancestors, descendants;
-        wallet.chain().getTransactionAncestry(output.outpoint.hash, ancestors, descendants);
+        size_t ancestors{0}, descendants{0};
+        if (output.m_mempool_info) {
+            ancestors = output.m_mempool_info->ancestors_count;
+            descendants = output.m_mempool_info->descendants_count;
+        }
         CScript spk = output.txout.scriptPubKey;
 
         std::vector<OutputGroup>& groups = spk_to_groups_map[spk];

2465eb8f15d5e406f53b85d1954de0ee273d57a7
furszy <matiasfurszyfer@protonmail.com>
2022-06-25 14:11:57 -0300

    refactor: result class, use std::variant internally
    As the result object is an OR, not an AND. Instead of two optional values, use std::variant.

--- src/result.h
+++ src/result.h
@@ -2,11 +2,12 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_OPERATIONRESULT_H
-#define BITCOIN_OPERATIONRESULT_H
+#ifndef BITCOIN_BRESULT_H
+#define BITCOIN_BRESULT_H
 
 #include <optional>
 #include <util/translation.h>
+#include <variant>
 
 /*
  * 'BResult' is a generic class useful for wrapping a return object
@@ -15,21 +16,24 @@
 template<class T>
 class BResult {
 private:
-    std::optional<T> m_obj_res{std::nullopt};
-    std::optional<bilingual_str> m_error{std::nullopt};
-public:
-    BResult() : m_error(Untranslated("")) {}
-    BResult(const T& _obj) : m_obj_res(_obj) {}
-    BResult(const bilingual_str& error) : m_error(error) {}
+    std::variant<bilingual_str, T> m_variant;
 
-    /* In case of failure, the error cause */
-    bilingual_str GetError() const { return (m_error ? *m_error : bilingual_str()); }
+public:
+    BResult() : m_variant(Untranslated("")) {}
+    BResult(const T& _obj) : m_variant(_obj) {}
+    BResult(const bilingual_str& error) : m_variant(error) {}
+
     /* In case of success, the result object */
-    const std::optional<T>& GetObj() const { return m_obj_res; }
+    const T* GetObj() const { return std::get_if<T>(&m_variant); }
     /* Whether the function succeeded or not */
-    bool GetRes() const { return m_error == std::nullopt; }
+    bool GetRes() const { return !std::holds_alternative<bilingual_str>(m_variant); }
+    /* In case of failure, the error cause */
+    bilingual_str GetError() const {
+        auto error = std::get_if<bilingual_str>(&m_variant);
+        return (error ? *error : bilingual_str());
+    }
 
     explicit operator bool() const { return GetRes(); }
 };
 
-#endif // BITCOIN_OPERATIONRESULT_H
+#endif // BITCOIN_BRESULT_H

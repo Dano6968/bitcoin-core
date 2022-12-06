abe2b010954fe6d1cae5825ee596985eb2d4c865
furszy <matiasfurszyfer@protonmail.com>
2022-12-06 12:39:00 -0300

    wallet: regression fix, getAvailableBalance skips selected coins
    Only a GUI issue, for the "use available balance" button when
    the user manually select inputs on the send screen.
    
    The previous behavior for getAvailableBalance when the coin
    control has selected inputs was to return the sum of them. Instead,
    we are currently returning the wallet's available total balance minus
    the selected coins total amount.
    
    Reason:
    We missed to update the GetAvailableBalance function to include
    the coin control selected coins on #25685.
    
    Context:
    Since #25685 we skip the selected coins inside `AvailableCoins`,
    the reason is that there is no need to walk through the entire
    wallet's txes map just to get coins that could have gotten by
    just doing a simple mapWallet.find).

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -353,12 +353,6 @@ CoinsResult AvailableCoinsListUnspent(const CWallet& wallet, const CCoinControl*
     return AvailableCoins(wallet, coinControl, /*feerate=*/ std::nullopt, params);
 }
 
-CAmount GetAvailableBalance(const CWallet& wallet, const CCoinControl* coinControl)
-{
-    LOCK(wallet.cs_wallet);
-    return AvailableCoins(wallet, coinControl).GetTotalAmount();
-}
-
 const CTxOut& FindNonChangeParentOutput(const CWallet& wallet, const CTransaction& tx, int output)
 {
     AssertLockHeld(wallet.cs_wallet);
